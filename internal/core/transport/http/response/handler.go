73c431c12479bad40aaec3f9a15e9f3d1f915c5a
avequa <11sssvvv11@gmail.com>
2026-07-08 23:14:23 +0300

    errors core

--- internal/core/transport/http/response/handler.go
+++ internal/core/transport/http/response/handler.go
@@ -2,13 +2,14 @@ package core_http_response
 
 import (
 	"fmt"
+	"errors"
 
 	"encoding/json"
 	"net/http"
 
-	"go.uber.org/zap"
+	core_errors "github.com/avequa/golang-todo-app/internal/core/errors"
 	core_logger "github.com/avequa/golang-todo-app/internal/core/logger"
-
+	"go.uber.org/zap"
 )
 
 type HTTPResponseHandler struct {
@@ -16,11 +17,50 @@ type HTTPResponseHandler struct {
 	rw http.ResponseWriter
 }
 
+func (h *HTTPResponseHandler) ErrorResponse(err error, msg string) {
+	var (
+		statusCode int
+		logFunc func(string, ...zap.Field)
+	)
+
+	switch {
+
+	case errors.Is(err, core_errors.ErrInvalidArgument):
+		statusCode = http.StatusBadRequest
+		logFunc = h.log.Warn
+
+	case errors.Is(err, core_errors.ErrNotFound):
+		statusCode = http.StatusNotFound
+		logFunc = h.log.Debug
+
+	case errors.Is(err, core_errors.ErrConflict):
+		statusCode = http.StatusConflict
+		logFunc = h.log.Warn
+
+	default:
+		statusCode = http.StatusInternalServerError
+		logFunc = h.log.Error
+	}
+}
+
 func (h *HTTPResponseHandler) PanicResponse(p any, msg string) {
 	statusCode := http.StatusInternalServerError
 	err := fmt.Errorf("panic: %v", p)
 
 	h.log.Error(msg, zap.Error(err))
+	
+	h.errorResponse(
+		statusCode,
+		err,
+		msg,
+	)
+}
+
+func (h *HTTPResponseHandler) errorResponse(
+	statusCode int, 
+	err error,
+	msg string,
+) {
 	h.rw.WriteHeader(statusCode)
 
 	response := map[string]string{
--- internal/features/users/transport/http/create_user.go
+++ internal/features/users/transport/http/create_user.go
@@ -1,14 +1,16 @@
 package users_transport_http
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
-	"encoding/json"
+
+	core_http_request "github.com/avequa/golang-todo-app/internal/core/transport/http/request"
 )
 
 type CreateUserRequest struct {
-	FullName string `json:"full_name"`
-	PhoneNumber *string `json:"phone_number"`
+	FullName string `json:"full_name" validate:"required,min=3,max=100"`
+	PhoneNumber *string `json:"phone_number" validate:"omitempty,min=3,max=100,startswith=+"`
 }
 
 type CreateUserResponse struct {
@@ -23,6 +25,10 @@ type CreateUserResponse struct {
 
 func (h *UsersHTTPHandler) CreateUser(rw http.ResponseWriter, r *http.Request) {
 	var request CreateUserRequest
+	if err := core_http_request.DecodeAndValidateRequest(r, &request); err != nil {
+		///
+	}
+
 	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
 		fmt.Println("ошибка")
 	}
