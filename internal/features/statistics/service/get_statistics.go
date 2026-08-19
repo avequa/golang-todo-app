e5edd0ef4f6667d96ff36b7831081388bfbe3340
avequa <11sssvvv11@gmail.com>
2026-08-19 20:33:23 +0300

    use new cons

--- internal/features/statistics/service/get_statistics.go
+++ internal/features/statistics/service/get_statistics.go
@@ -41,12 +41,7 @@ func (s *StatisticsService) GetStatistics(
 func calcStatistics(tasks []domain.Task) domain.Statistics {
 
 	if len(tasks) == 0 {
-		return domain.Statistics{
-			TasksCreated:               0,
-			TasksCompleted:             0,
-			TasksCompletedRate:         nil,
-			TasksAverageCompletionTime: nil,
-		}
+		return domain.NewStatistics(0, 0, nil, nil)
 	}
 
 	tasksCreated := len(tasks)
@@ -74,11 +69,11 @@ func calcStatistics(tasks []domain.Task) domain.Statistics {
 		tasksAverageCompletionTime = &avg
 	}
 
-	return domain.Statistics{
-		TasksCreated:               tasksCreated,
-		TasksCompleted:             tasksCompleted,
-		TasksCompletedRate:         &tasksCompletedRate,
-		TasksAverageCompletionTime: tasksAverageCompletionTime,
-	}
+	return domain.NewStatistics(
+		tasksCreated,
+		tasksCompleted,
+		&tasksCompletedRate,
+		tasksAverageCompletionTime,
+	)
 
 }
